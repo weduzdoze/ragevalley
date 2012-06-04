32821de610c0125353222efce698c079fd32440a
unknown <Pete@Pete-PC.(none)>
2012-06-03 22:39:39 -0400

    modify header styling

--- view/dsp_header.php
+++ view/dsp_header.php
@@ -4,23 +4,18 @@
 
 <link href="resources/css/jquery-ui-1.8.17.custom.css" rel="stylesheet" type="text/css">
 <link href="resources/css/main.css" rel="stylesheet" type="text/css">
-<link rel="stylesheet" type="text/css" href="resources/css/admin.css"/>
 <script type="text/javascript" src="resources/js/jquery-1.7.1.min.js"></script>
 <script type="text/javascript" src="resources/js/jquery-ui.min.js"></script>
 
-<div id="header">
-	
+<div id="header">	
 	<img src="resources/images/headerPhoto.png" align="center" class="header"/>
-	<br>
+	
+	<div class="menu">
 	<a href="index.php?action=home" class="headerLink">Home</a>
 	<a href="index.php?action=events" class="headerLink">Events</a>
 	<a href="" class="headerLink">About Us</a>
 	<a href="" class="headerLink">Contact Us</a>
-	<br><br>
-
 	</div>
-
-
-
+</div>
 
 <div class="container">
