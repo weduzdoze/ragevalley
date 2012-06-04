4251565bece7e0a94014cca5d0c540c0341b4555
Christina Bethencourt <bethenc@gmail.com>
2012-06-04 18:23:18 -0400

    style eventDetails

--- view/dsp_eventDetails.php
+++ view/dsp_eventDetails.php
@@ -1,25 +1,28 @@
-<div id="eventDetailsEventName"><h1><?php echo $event->getName();?></h1></div>
-<div id="eventDetailsFeaturing"><h2>Featuring:</h2></div>
-<div id="eventDetailsArtist"><h1><?php echo $event->artist->getName();?></h1>
+<div id="eventDetailsEventName"><?php echo $event->getName();?>!</div><br>
+<div id="eventDetailsFeaturing"><i>Featuring:</i></div><br>
+<div id="eventDetailsArtistName"><?php echo $event->artist->getName();?></div>
+<div id="eventDetailsArtist">
 <?php 
 	$imageFileNameLength = strlen($event->getImageFileName());
 	if ($imageFileNameLength > 1){
-		echo "<img src='images/events/" . $event->getImageFileName() . "' class=''/>";
+		echo "<img src='images/events/" . $event->getImageFileName() . "' class='imagee'>";
 		echo "<br />";
 	}
 ?>
 
-		<span id="eventName">Name: <?php echo $event->getName();?></span><br />
-		<span id="eventArtist">Artist: <?php echo $event->artist->getName();?></span><br />	
-		<span id="eventStart">Start: <?php echo $event->getStart();?></span><br />
-		<span id="eventEnd">End: <?php echo $event->getEnd();?></span><br />
-		<span id="eventVenue">Venue: <?php echo $event->venue->getName();?></span><br />
-		<span id="eventGenre">Genre: <?php echo $event->genre->getName();?></span><br />
-		<span id="eventAge">Age: <?php echo $event->age->getName();?></span><br />
-		<span id="eventPrice">Price: <?php echo $event->getPrice();?></span><br />
+		<span id="eventName"><b>Name: </b><?php echo $event->getName();?></span><br />
+		<span id="eventArtist"><b>Artist: </b><?php echo $event->artist->getName();?></span><br />	
+		<span id="eventStart"><b>Start: </b><?php echo $event->getStart();?></span><br />
+		<span id="eventEnd"><b>End: </b><?php echo $event->getEnd();?></span><br />
+		<span id="eventVenue"><b>Venue: </b><?php echo $event->venue->getName();?></span><br />
+		<span id="eventGenre"><b>Genre: </b><?php echo $event->genre->getName();?></span><br />
+		<span id="eventAge"><b>Age: </b><?php echo $event->age->getName();?></span><br />
+		<span id="eventPrice"><b>Price: </b><?php echo $event->getPrice();?></span><br />
 		<?php 
 		$facebookLength = strlen($event->getFacebook());
 		if ($facebookLength > 1){ ?>
-			<span id="eventFacebook">Facebook: <?php echo $event->getFacebook();?></span><br />
+			<span id="eventFacebook"><b>Facebook:</b> <?php echo $event->getFacebook();?></span><br />
 		<?php } ?>		
-		<span id="eventDetails">Details: <?php echo $event->getDetails();?></span><br />
+		<span id="eventDetails"><b>Details:</b> <?php echo $event->getDetails();?></span><br />
+		
+</div>
