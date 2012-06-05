0f3e2197ed43c29dc5c119fb3d73dce0a4fdec89
Christina Bethencourt <bethenc@gmail.com>
2012-06-04 22:07:44 -0400

    new images and about us and home

---
+++ view/dsp_aboutUs.php
@@ -0,0 +1,6 @@
+<br/><br/>
+
+
+<img src="images/1.png"><img src="images/2.png"><br/>
+
+This is Christina and Pete's Info 153 final project. 
---
+++ view/dsp_home.php
@@ -0,0 +1,20 @@
+<br/>
+
+
+<img src="images/home1.png" class="image"><br/><br/>
+
+
+<b>Love the EDM scene? <br/>
+Trying to find a show to rage at? </b><br/>
+You've come to the right place.<br/>
+This site provides you with upcoming shows and festivals in your area.<br/>
+Click the Events link in the navigation bar above to get started.<br/><br/><br/><br/><br/>
+
+
+
+
+<i>If you would like to purchase any tickets to the events found on this site, <br/>
+you are able to at the ticket providers linked below.</i><br/><br/>
+
+<a href="http://www.livenation.com"><img src="images/tickets.png"></a><br/>
+<a href="http://www.ticketfly.com"><img src="images/ticketfly.png"></a>
